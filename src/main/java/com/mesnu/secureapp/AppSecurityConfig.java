b6a9cf254b988ca2d428f7562af49aebf02cab8b
mesnuuu <mesnuuu@gmail.com>
2021-03-22 17:09:44 +0500

    1.2.1 - Verification from database
    Dependencies
    
    1. Spring Boot Starter Data JPA -
    2. MySQL Connector/J » 8.0.22 -
    
    **************************************
    use springsecurityDb;
    
    CREATE TABLE user (
        id int NOT NULL AUTO_INCREMENT,
        username varchar(255),
        password varchar(255),
        PRIMARY KEY (id)
    );
    
    insert into user (username, password)
    values ('Spiderman','web'),
     ('Superman','fly');
    
    select * from user;

--- src/main/java/com/mesnu/secureapp/AppSecurityConfig.java
+++ src/main/java/com/mesnu/secureapp/AppSecurityConfig.java
@@ -3,36 +3,51 @@ package com.mesnu.secureapp;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService ;
+	
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
+	public AuthenticationProvider authProvider()
+	{
 		
-		// in memory username and password
-		// userdetails is inbuilt class in spring security
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		
-		List<UserDetails> users = new ArrayList<>();
-		//
-		users.add(User.withDefaultPasswordEncoder().username("John").password("1234").roles("USER").build());
+		provider.setUserDetailsService(userDetailsService);
 		
-		return new InMemoryUserDetailsManager(users);
+		// Because experimenting
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 		
-		
-		
+		return provider;
 	}
+	
+	/*
+	 * @Bean
+	 * 
+	 * @Override protected UserDetailsService userDetailsService() { // in memory
+	 * username and password
+	 * 
+	 * List<UserDetails> users = new ArrayList<>();
+	 * users.add(User.withDefaultPasswordEncoder().username("John").password("1234")
+	 * .roles("USER").build());
+	 * 
+	 * return new InMemoryUserDetailsManager(users); }
+	 */
 
-	
-	
 }
---
+++ src/main/java/com/mesnu/secureapp/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mesnu.secureapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>
+
+{
+
+	User findbyUsername(String username);
+	
+}
